621110cfce4f7c06713d2a1f8aabdacfd49e084f
dsokolov <d.sokolov@weborama.com.ru>
2022-07-12 18:31:47 +0300

    domain work

--- run.py
+++ run.py
@@ -21,7 +21,11 @@ raw = 1
 column = 0
 test = 0
 raw_prev = 1
-diff = 0
+
+worksheet.write(0, 0, "domain")
+worksheet.write(0, 1, "site")
+worksheet.write(0, 2, "firewall")
+worksheet.write(0, 3, "category")
 
 merge_format = workbook.add_format({
     'bold': 1,
@@ -34,27 +38,26 @@ if my_file:
     current_line = my_file.readline()
 
 first_line_value = current_line.split(",")
-for i in first_line_value:
-    if column == 2:
-        column += 1
-    worksheet.write(raw, column, i)
-    column += 1
+first_line_domain = first_line_value[0]
+first_line_url = first_line_value[1]
+first_line_value = first_line_value[2]
 
 for line in my_file:
+    raw += 1
     previous_line = current_line
     current_line = line
 
     get = do_stuff_with_two_lines(previous_line, current_line)
 
     for i in get:
-        if i == 0:
-            raw += 1
-            raw_prev += 1
-        else:
+        if i != 0:
             print(raw_prev)
             print(raw)
             print(i[0])
-            worksheet.merge_range(raw_prev, 0, raw, 0, i[0], merge_format)
+            if raw_prev == 1:
+                worksheet.merge_range(raw_prev, 0, raw, 0, first_line_domain, merge_format)
+            else:
+                worksheet.merge_range(raw_prev, 0, raw, 0, i[0], merge_format)
             raw_prev = raw + 1
 
     raw += 1
